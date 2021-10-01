b36e4965cabd08f66caf8ad85e8c69d52bdf14fa
GeorgeFritz8 <30572@ma-web.nl>
2021-10-01 15:20:00 +0200

    Beetje extra commit

--- Assets/Scripts/HealthComponent.cs
+++ Assets/Scripts/HealthComponent.cs
@@ -9,7 +9,31 @@ public class HealthComponent : MonoBehaviour
 
     private void Start()
     {
-        _currentHealth = _startHealth
+        _currentHealth = _startHealth;  
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5);
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        print("ik krijg damage");
+        _currentHealth -= amount;
+
+        if (_currentHealth <= 0)
+        {
+            Death();    
+        }
+
+    }
+
+    private void Death()
+    {
+        print("ik ben dood");
+    }
 }
