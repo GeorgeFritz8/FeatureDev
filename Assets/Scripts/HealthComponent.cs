fdf342810395ed8305e32a422b989bc23e389da6
GeorgeFritz8 <30572@ma-web.nl>
2021-10-01 12:22:35 +0200

    Opdracht commit

---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    [SerializeField] private float _currentHealth;
+    private float _startHealth;
+
+    private void Start()
+    {
+        _currentHealth = _startHealth
+    }
+
+}
